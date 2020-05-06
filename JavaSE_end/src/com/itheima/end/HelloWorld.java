99dab45af6b2a207f31f4bdb51e6e65ba34eb6f7
Denny <811968135@qq.com>
2020-05-07 00:17:08 +0800

    更新类

--- JavaSE_end/src/com/itheima/end/HelloWorld.java
+++ JavaSE_end/src/com/itheima/end/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("夯哥,有缘再见!");
+        System.out.println("夯哥,江湖路远,有缘再见!");
     }
 }
